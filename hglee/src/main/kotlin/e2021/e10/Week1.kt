ee2bf616cbf197efc22e5292ccfad85d94807ae8
hyungyu-Lee <gusrb0808@naver.com>
2021-09-29 18:23:21 +0900

    코드 수정

--- hglee/src/main/kotlin/e2021/e10/Week1.kt
+++ hglee/src/main/kotlin/e2021/e10/Week1.kt
@@ -5,16 +5,17 @@ import java.util.*
 fun main(args: Array<String>) = with(Scanner(System.`in`)) {
     val n = nextInt()
     val k = nextInt()
-    val list = mutableListOf<Int>()
-    for (i in 0 until n) {
-        list.add(nextInt())
-    }
     val diff = mutableListOf<Int>()
-    var prev = list[0]
-    for (i in 1..list.lastIndex) {
-        val now = list[i]
+    var prev = nextInt()
+    for (i in 1 until n) {
+        val now = nextInt();
         diff.add(now - prev)
         prev = now
     }
-    println(diff.sorted().subList(0, n - k).sum())
+    diff.sort()
+    var sum = 0
+    for (i in 0 until n - k) {
+        sum += diff[i]
+    }
+    println(sum)
 }
