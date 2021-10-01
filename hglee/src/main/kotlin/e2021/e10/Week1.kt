a54d9592f9443de623515939d94174dff7c3f837
hyungyu-Lee <gusrb0808@naver.com>
2021-10-01 23:15:11 +0900

    1주차 문제 코드 수정

--- hglee/src/main/kotlin/e2021/e10/Week1.kt
+++ hglee/src/main/kotlin/e2021/e10/Week1.kt
@@ -5,12 +5,21 @@ import java.util.*
 fun main(args: Array<String>) = with(Scanner(System.`in`)) {
     val n = nextInt()
     val k = nextInt()
+    if (n == k) {
+        println(0)
+        return
+    }
     val diff = mutableListOf<Int>()
-    var prev = nextInt()
+    var first = nextInt()
+    var prev = first
     for (i in 1 until n) {
         val now = nextInt();
         diff.add(now - prev)
         prev = now
+    }
+    if (k == 1) {
+        println(prev - first)
+        return
     }
     diff.sort()
     var sum = 0
