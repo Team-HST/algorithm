939e2d87b957e45a066690f378e96dde3f187a50
dudgns0612 <dudgns0612@naver.com>
2021-11-03 22:49:43 +0900

    2021-10 week3 테스트

---
+++ yhkim/prevAlgorithm/2021-10-week3.js
@@ -0,0 +1,48 @@
+const MinHeap = require('./prevAlgorithm/MinHeap');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let count = 0;
+const minHeap = new MinHeap();
+rl.on('line', function (line) {
+  const input = Number(line);
+  if (!count) {
+    count = input;
+  } else {
+    minHeap.add(input);
+    if (minHeap.getHeapSize() === count) {
+      solution(minHeap);
+    }
+  }
+}).on('close', function () {
+  process.exit();
+});
+
+function solution(minHeap) {
+  let result = 0;
+  if (minHeap.getHeapSize() === 2) {
+    result += minHeap.poll();
+    result += minHeap.poll();
+    console.log(result);
+    return;
+  }
+
+  while (minHeap.getHeapSize()) {
+    let sum = 0;
+    sum += minHeap.poll();
+    sum += minHeap.poll();
+    result += sum;
+
+    minHeap.add(sum);
+
+    if (minHeap.getHeapSize() === 2) {
+      result += minHeap.poll();
+      result += minHeap.poll();
+    }
+  }
+
+  console.log(result);
+}
